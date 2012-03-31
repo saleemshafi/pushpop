bc2bff84f241123363bf4e2c924f064aa42e6cf0
Saleem Shafi <saleemshafi@gmail.com>
2012-03-30 22:40:43 -0500

    added timer functionality

--- script/touchpad.js
+++ script/touchpad.js
@@ -1,10 +1,16 @@
 enyo.kind({
   name: "MyApps.PushPop",
   kind: "Control",
+  game: null,
   components: [
                {name: "game-stack", classes:"stack"},
                {name: "game-board", classes:"game_board"},
                {name: "solution", classes:"stack"},
+               {name: "timerPanel", classes:"timer", components: [
+               		{name: "timer"},
+               		{name: "pauseButton", kind: "Button", caption:"Pause", onclick: "pauseTimer"},
+               		{name: "resumeButton", kind: "Button", caption:"Resume", onclick: "resumeTimer"}
+               ]},
 			   {kind: "AppMenu",
 					  components: [
 					  	// Ctrl+~
@@ -13,9 +19,25 @@ enyo.kind({
 					  ]
 				}
              ],
+  create: function() {
+	this.inherited(arguments);
+  	this.game = genGame(4,4);
+  },
+  rendered: function() {
+  	this.game.start();
+  },
   windowRotated: function() {},
   newPuzzle: function() {
-  	genGame(4,4).render();
+  	this.game = genGame(4,4);
+  	this.game.start();
+  },
+  pauseTimer: function() {
+  	this.game.timer.pause();
+  	$("#pushPop_timerPanel").addClass("paused");
+  },
+  resumeTimer: function() {
+  	$("#pushPop_timerPanel").removeClass("paused");
+  	this.game.timer.start();
   },
   showPreferences: function() {
 	alert("prefs");
